029fa87bff032aa73f277067894b979103d18307
Valentin Makarov <89makarov@list.ru>
2015-09-12 22:50:13 +0600

    Balance theoretically may be the Singleton, add appropriate changes to that class. Add the test marking for new article activity or fragment. Add required strings resources.

--- app/src/main/java/ru/auvvinc/homeaccounting/Balance.java
+++ app/src/main/java/ru/auvvinc/homeaccounting/Balance.java
@@ -4,4 +4,21 @@ package ru.auvvinc.homeaccounting;
  * Created by alterpunk on 12.09.2015.
  */
 public class Balance {
+
+    private static Balance instance;
+
+    public static void initInstance() {
+        if (instance == null) {
+            instance = new Balance();
+        }
+    }
+
+    public static Balance getInstance() {
+        return instance;
+    }
+
+    private Balance() {
+
+    }
+
 }
